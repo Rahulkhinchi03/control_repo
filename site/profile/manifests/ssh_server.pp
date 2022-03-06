def2b2154b4c35e9d7584975453fcf0b576d6757
Rahul Khinchi <71710042+Rahulkhinchi03@users.noreply.github.com>
2022-03-06 17:18:27 +0530

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC8eLrrSjE/nvOOOxMEIpDhHWf8WybE2z6pEOzmcEAjFR/xwOyum/gj3h4sPaByME29RLz5VmdD2Zg4gg0SyxWDQnuqDORRkgQD+P/ttA/x/svPDY9YdDhn7EXCx+6zKXdDu6D+5GTdBgWp+VkSS7o+tvxeBjCgvvlRIfffP+WoFy/ytqkyT9zzPmSLnw++wkbfbyIUfkY9VwXwoK0gP3wcFFB6jBr2IwJB9jAJJd4lopOuOJxxHnuu+yE51yKEx/7uZHOvaMPVspQDCi0DSARIiJjSTYS0Oa3ABNxpMA0z41ZQi1RPb0v/RwaDxeo2cr9ydz610ulHICiMguhOMIz5 root@master.puppet.vm',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC8eLrrSjE/nvOOOxMEIpDhHWf8WybE2z6pEOzmcEAjFR/xwOyum/gj3h4sPaByME29RLz5VmdD2Zg4gg0SyxWDQnuqDORRkgQD+P/ttA/x/svPDY9YdDhn7EXCx+6zKXdDu6D+5GTdBgWp+VkSS7o+tvxeBjCgvvlRIfffP+WoFy/ytqkyT9zzPmSLnw++wkbfbyIUfkY9VwXwoK0gP3wcFFB6jBr2IwJB9jAJJd4lopOuOJxxHnuu+yE51yKEx/7uZHOvaMPVspQDCi0DSARIiJjSTYS0Oa3ABNxpMA0z41ZQi1RPb0v/RwaDxeo2cr9ydz610ulHICiMguhOMIz5root@master.puppet.vm',
   }  
 }
